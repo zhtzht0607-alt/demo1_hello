11995a4b440e7735ecee5a0f5b46cd57f4e96130
zhtzht0607-alt <zhtzht0607@gmail.com>
2026-06-02 22:25:36 +0800

    add

---
+++ src/main/java/org/example/ctrl/controller.java
@@ -0,0 +1,13 @@
+package org.example.ctrl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class controller {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hellospring";
+    }
+}
