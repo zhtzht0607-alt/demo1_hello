c39dabb9fa3165af216e822ffca389b2d575fba6
zhtzht0607-alt <zhtzht0607@gmail.com>
2026-06-03 20:27:35 +0800

    add2

--- src/main/java/org/example/ctrl/controller.java
+++ src/main/java/org/example/ctrl/controller.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class controller {
     @RequestMapping("/hello")
-    public String hello(){
-        return "hellospring";
+    public String hello() {
+        return "乔鸿";
     }
 }
